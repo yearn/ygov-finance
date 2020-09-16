a77d5bfa6a400d9a68aa84242a3db6204f58822c
Pemba Gelu Sherpa <pemba.sherpa5232@gmail.com>
2020-09-16 04:28:48 +0400

    feat(Vote): Vote Page for desktop.
    - Feature Implementation for Vote page
    - Proposals list and staking functions.

--- src/theme/theme.jsx
+++ src/theme/theme.jsx
@@ -1,11 +1,11 @@
-import createBreakpoints from '@material-ui/core/styles/createBreakpoints'
+import createBreakpoints from "@material-ui/core/styles/createBreakpoints";
 
-import WorkSansTTF from '../assets/fonts/WorkSans-VariableFont_wght.ttf';
+import WorkSansTTF from "../assets/fonts/WorkSans-VariableFont_wght.ttf";
 
 const WorkSans = {
-  fontFamily: 'Work Sans Thin',
-  fontStyle: 'normal',
-  fontDisplay: 'swap',
+  fontFamily: "Work Sans Thin",
+  fontStyle: "normal",
+  fontDisplay: "swap",
   fontWeight: 400,
   src: `
     local('Work Sans Thin'),
@@ -13,36 +13,50 @@ const WorkSans = {
     url(${WorkSansTTF}) format('truetype')
   `,
   unicodeRange:
-    'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
+    "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF",
 };
 
 export const colors = {
   white: "#fff",
-  black: '#000',
+  black: "#000",
   blackBlue: "#1e272f",
   blue: "#2F80ED",
   gray: "#e1e1e1",
   lightGray: "#737373",
   lightBlack: "#6a6a6a",
   darkBlack: "#141414",
-  green: '#1abc9c',
-  red: '#ed4337',
-  orange: 'orange',
-  pink: '#DC6BE5',
-  compoundGreen: '#00d395',
-  tomato: '#e56b73',
-  purple: '#935dff',
+  green: "#1abc9c",
+  red: "#ed4337",
+  orange: "orange",
+  pink: "#DC6BE5",
+  compoundGreen: "#00d395",
+  tomato: "#e56b73",
+  purple: "#935dff",
 
   text: "#212529",
   lightBlue: "#2F80ED",
   topaz: "#0b8f92",
   darkGray: "rgba(43,57,84,.5)",
-  borderBlue: 'rgba(25, 101, 233, 0.5)',
-  greyBackground: '#2B3A4A',
-  greyText: '#BDCBDA',
-  yellowBackground: 'rgba(238, 203, 112, 0.3)',
-  yellowText: '#EECB70',
-  blueGreyText: '#BDCBDA',
+  borderBlue: "rgba(25, 101, 233, 0.5)",
+  greyBackground: "#2B3A4A",
+  greyText: "#BDCBDA",
+  yellowBackground: "rgba(238, 203, 112, 0.3)",
+  yellowText: "#EECB70",
+  blueGreyText: "#BDCBDA",
+
+  transGreyBackground: "rgba(255, 255, 255, 0.2)",
+  transGreyBackgroundHover: "rgba(255, 255, 255, 0.4)",
+  lightGreen: "#8BFF55",
+
+  darkGray2: "#3a4754",
+  lightGray2: "#41505d",
+  lightGray3: "#54616d",
+  lightGray4: "#56626f",
+  lightGray5: "#67727d",
+  brandBlue: "#2A5BDB",
+
+  lightGreen2: "#A0FFC1",
+  lightRed: "#FFA0A0",
 };
 
 const breakpoints = createBreakpoints({
@@ -52,270 +66,271 @@ const breakpoints = createBreakpoints({
     sm: 600,
     md: 900,
     lg: 1200,
-    xl: 1800
-  }
-})
+    xl: 1800,
+  },
+});
 
-const iswapTheme =  {
+const iswapTheme = {
   typography: {
     fontFamily: [
       '"Work Sans Thin"',
-      'BlinkMacSystemFont',
+      "BlinkMacSystemFont",
       '"Segoe UI"',
-      'Roboto',
+      "Roboto",
       '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
+      "Arial",
+      "sans-serif",
       '"Apple Color Emoji"',
       '"Segoe UI Emoji"',
       '"Segoe UI Symbol"',
       '"Formular"',
-    ].join(','),
+    ].join(","),
     h1: {
-      fontSize: '48px',
-      fontWeight: '600',
+      fontSize: "48px",
+      fontWeight: "600",
       WebkitFontSmoothing: "antialiased",
       MozOsxFontSmoothing: "grayscale",
-      lineHeight: 1.2
+      lineHeight: 1.2,
     },
     h2: {
-      fontSize: '36px',
-      fontWeight: '600',
+      fontSize: "36px",
+      fontWeight: "600",
       WebkitFontSmoothing: "antialiased",
       MozOsxFontSmoothing: "grayscale",
-      lineHeight: 1.2
+      lineHeight: 1.2,
     },
     h3: {
-      fontSize: '24px',
-      fontWeight: '600',
+      fontSize: "24px",
+      fontWeight: "600",
       WebkitFontSmoothing: "antialiased",
       MozOsxFontSmoothing: "grayscale",
-      lineHeight: 1.2
+      lineHeight: 1.2,
     },
     h4: {
-      fontSize: '16px',
-      fontWeight: '600',
+      fontSize: "16px",
+      fontWeight: "600",
       WebkitFontSmoothing: "antialiased",
       MozOsxFontSmoothing: "grayscale",
-      lineHeight: 1.2
+      lineHeight: 1.2,
     },
     h5: {
-      fontSize: '14px',
-      fontWeight: '600',
+      fontSize: "14px",
+      fontWeight: "600",
       WebkitFontSmoothing: "antialiased",
       MozOsxFontSmoothing: "grayscale",
-      lineHeight: 1.2
+      lineHeight: 1.2,
     },
     body1: {
-      fontSize: '16px',
-      fontWeight: '300',
+      fontSize: "16px",
+      fontWeight: "300",
       WebkitFontSmoothing: "antialiased",
       MozOsxFontSmoothing: "grayscale",
     },
     body2: {
-      fontSize: '16px',
-      fontWeight: '300',
+      fontSize: "16px",
+      fontWeight: "300",
       WebkitFontSmoothing: "antialiased",
       MozOsxFontSmoothing: "grayscale",
     },
     body3: {
-      fontFamily: 'Formular',
-      fontStyle: 'normal',
-      fontWeight: 'normal',
-      fontSize: '24px',
-      lineHeight: '29px',
-      letterSpacing: '0.3em'
-    }
+      fontFamily: "Formular",
+      fontStyle: "normal",
+      fontWeight: "normal",
+      fontSize: "24px",
+      lineHeight: "29px",
+      letterSpacing: "0.3em",
+    },
   },
-  type: 'light',
+  type: "light",
   overrides: {
     MuiCssBaseline: {
-      '@global': {
-        '@font-face': [WorkSans],
+      "@global": {
+        "@font-face": [WorkSans],
       },
     },
     MuiSelect: {
       select: {
-        padding: '9px'
+        padding: "9px",
       },
       selectMenu: {
-        minHeight: '30px',
-        display: 'flex',
-        alignItems: 'center'
-      }
+        minHeight: "30px",
+        display: "flex",
+        alignItems: "center",
+      },
     },
     MuiButton: {
       root: {
-        borderRadius: '50px',
-        padding: '10px 24px',
-        background: 'white'
+        borderRadius: "50px",
+        padding: "10px 24px",
+        background: "white",
       },
       outlined: {
-        padding: '10px 24px',
-        borderWidth: '2px !important'
+        padding: "10px 24px",
+        borderWidth: "2px !important",
       },
       text: {
-        padding: '10px 24px'
+        padding: "10px 24px",
       },
       label: {
-        textTransform: 'none',
-        fontSize: '1rem'
-      }
+        textTransform: "none",
+        fontSize: "1rem",
+      },
     },
     MuiInputBase: {
       input: {
-        fontSize: '16px',
-        fontWeight: '600',
+        fontSize: "16px",
+        fontWeight: "600",
         WebkitFontSmoothing: "antialiased",
         MozOsxFontSmoothing: "grayscale",
-        lineHeight: 1.2
-      }
+        lineHeight: 1.2,
+      },
     },
     MuiOutlinedInput: {
-      adornedEnd: {
-
-      },
+      adornedEnd: {},
       input: {
         "&::placeholder": {
-          color: colors.text
+          color: colors.text,
         },
         color: colors.text,
-        padding: '14px',
+        padding: "14px",
       },
       root: {
         // border: "none !important",
-        borderRadius: '25px'
+        borderRadius: "25px",
       },
       notchedOutline: {
         // border: "none !important"
-      }
+      },
     },
-    MuiSnackbar : {
+    MuiSnackbar: {
       root: {
-        maxWidth: 'calc(100vw - 24px)'
+        maxWidth: "calc(100vw - 24px)",
       },
       anchorOriginBottomLeft: {
-        bottom: '12px',
-        left: '12px',
-        '@media (min-width: 960px)': {
-          bottom: '50px',
-          left: '80px'
-        }
-      }
+        bottom: "12px",
+        left: "12px",
+        "@media (min-width: 960px)": {
+          bottom: "50px",
+          left: "80px",
+        },
+      },
     },
     MuiSnackbarContent: {
       root: {
         backgroundColor: colors.white,
-        padding: '0px',
-        minWidth: 'auto',
-        '@media (min-width: 960px)': {
-          minWidth: '500px',
-        }
+        padding: "0px",
+        minWidth: "auto",
+        "@media (min-width: 960px)": {
+          minWidth: "500px",
+        },
       },
       message: {
-        padding: '0px'
+        padding: "0px",
       },
       action: {
-        marginRight: '0px'
-      }
+        marginRight: "0px",
+      },
     },
-    MuiAccordion: {
+    MuiExpansionPanel: {
       root: {
-        border: '1px solid '+colors.borderBlue,
-        borderRadius: '50px',
-        margin: '8px 0px',
-        '&:before': { //underline color when textfield is inactive
-          backgroundColor: 'none',
-          height: '0px'
+        border: "1px solid " + colors.borderBlue,
+        borderRadius: "50px",
+        margin: "8px 0px",
+        "&:before": {
+          //underline color when textfield is inactive
+          backgroundColor: "none",
+          height: "0px",
         },
-      }
+      },
     },
     MuiInput: {
       underline: {
-        '&:before': { //underline color when textfield is inactive
-          borderBottom: 'none !important'
+        "&:before": {
+          //underline color when textfield is inactive
+          borderBottom: "none !important",
         },
-        '&:after': { //underline color when textfield is inactive
-          borderBottom: 'none !important'
+        "&:after": {
+          //underline color when textfield is inactive
+          borderBottom: "none !important",
         },
-      }
+      },
     },
-    MuiAccordianSummary: {
+    MuiExpansionPanelSummary: {
       root: {
-        padding: '30px 42px'
+        padding: "30px 42px",
       },
       content: {
-        margin: '0px !important'
-      }
+        margin: "0px !important",
+      },
     },
-    MuiAccordianDetails: {
+    MuiExpansionPanelDetails: {
       root: {
-        padding: '0 12px 15px 12px',
-        '@media (min-width: 960px)': {
-          padding: '0 24px 30px 24px',
-        }
-      }
+        padding: "0 12px 15px 12px",
+        "@media (min-width: 960px)": {
+          padding: "0 24px 30px 24px",
+        },
+      },
     },
     MuiToggleButton: {
       root: {
-        borderRadius: '50px',
-        textTransform: 'none',
-        minWidth:  '100px',
-        background: 'white',
-        border: 'none',
-        '& > span > h4': {
-          color: '#555',
+        borderRadius: "50px",
+        textTransform: "none",
+        minWidth: "100px",
+        background: "white",
+        border: "none",
+        "& > span > h4": {
+          color: "#555",
         },
-        '&:hover': {
+        "&:hover": {
           backgroundColor: "rgba(47,128,237, 0.2)",
         },
         "&$selected": {
-          backgroundColor: '#2f80ed',
-          '& > span > h4': {
-            color: '#fff',
+          backgroundColor: "#2f80ed",
+          "& > span > h4": {
+            color: "#fff",
           },
-          '&:hover': {
+          "&:hover": {
             backgroundColor: "rgba(47,128,237, 0.2)",
-            '& > span > h4': {
-              color: '#000',
+            "& > span > h4": {
+              color: "#000",
             },
           },
-        }
-      }
+        },
+      },
     },
     MuiPaper: {
       elevation1: {
-        boxShadow: 'none'
-      }
+        boxShadow: "none",
+      },
     },
     MuiToggleButtonGroup: {
       root: {
-        border: '1px solid '+colors.borderBlue,
-        borderRadius: '50px',
+        border: "1px solid " + colors.borderBlue,
+        borderRadius: "50px",
       },
-      // groupedSizeSmall: {
-      //   padding: '42px 30px'
-      // }
+      groupedSizeSmall: {
+        padding: "42px 30px",
+      },
     },
     MuiFormControlLabel: {
       label: {
-        color: colors.darkBlack
-      }
-    }
+        color: colors.darkBlack,
+      },
+    },
   },
   palette: {
     primary: {
-      main: colors.blue
+      main: colors.blue,
     },
     secondary: {
-      main: colors.topaz
+      main: colors.topaz,
     },
     text: {
       primary: colors.text,
-      secondary: colors.text
-    }
+      secondary: colors.text,
+    },
   },
-  breakpoints: breakpoints
+  breakpoints: breakpoints,
 };
 
 export default iswapTheme;
